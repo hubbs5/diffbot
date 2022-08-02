d2a1a01c2353b8b77338c1c98deb1f391f6522f4
Christian <christiandhubbs@gmail.com>
2022-08-02 07:58:01 -0500

    added buildOrganizationQuery

--- diffbot/utils.py
+++ diffbot/utils.py
@@ -4,17 +4,20 @@ KG_BASE_URL = "https://kg.diffbot.com/kg/v3/dql"
 ARTICLE_BASE_URL = "https://api.diffbot.com/v3/article"
 ENHANCE_BASE_URL = "https://kg.diffbot.com/kg/v3/enhance_endpoint"
 
-def _buildKnowledgeGraphQuery(filter: str, token: str, type: str="query", size: int=50):
-    url = f"{KG_BASE_URL}?type={type}&token={token}&query={filter}&size={size}"
+def _buildKnowledgeGraphQuery(_filter: str, token: str, type: str="query", size: int=50):
+    url = f"{KG_BASE_URL}?type={type}&token={token}&query={_filter}&size={size}"
     return url
 
-def buildOrganizationQuery():
-    pass
 
-def buildArticleQuery(filter: str, token: str, type: str="query", size: int=50):
-    filter = "type: Article " + filter
-    return _buildKnowledgeGraphQuery(filter, token, type, size)
+def buildOrganizationQuery(_filter: str, token: str, type: str="query", size: int=50):
+    _filter = "type:Organization " + _filter
+    return _buildKnowledgeGraphQuery(_filter, token, type, size)
 
 
-def buildExtractArticleQuery(filter: str, token: str, type: str="query"):
+def buildArticleQuery(_filter: str, token: str, type: str="query", size: int=50):
+    _filter = "type:Article " + _filter
+    return _buildKnowledgeGraphQuery(_filter, token, type, size)
+
+
+def buildExtractArticleQuery(_filter: str, token: str, type: str="query"):
     pass
