3ff431a521e7613c67c3f412add3e82a5507aef0
Christian <christiandhubbs@gmail.com>
2023-11-08 14:56:49 -0600

    updated knowledge graph apis

---
+++ diffbot/knowledge_graph/__init__.py
@@ -0,0 +1,40 @@
+import urllib.parse
+
+# https://docs.diffbot.com/kgapi
+# https://docs.diffbot.com/docs/en/dql-quickstart
+KG_BASE_URL = "https://kg.diffbot.com/kg/v3/dql"
+ARTICLE_BASE_URL = "https://api.diffbot.com/v3/article"
+ENHANCE_BASE_URL = "https://kg.diffbot.com/kg/v3/enhance_endpoint"
+
+def build_knowledge_graph_query(
+    _filter: str, token: str, type: str = "query", size: int = 50
+    ):
+    _url = urllib.parse.quote(f"?type={type}&token={token}&query={_filter}&size={size}")
+    url = f"{KG_BASE_URL}{_url}"
+    return url
+
+
+def build_knowledge_graph_post_query(
+    _filter: str, token: str, type: str = "query", size: int = 50
+):
+    url = f"{KG_BASE_URL}?token={token}"
+    payload = {
+        "query": _filter,
+        "type": type,
+        "size": size,
+    }
+    return url, payload
+
+
+def build_organization_query(
+    _filter: str, token: str, type: str = "query", size: int = 50
+):
+    _filter = "type:Organization " + _filter
+    return build_knowledge_graph_query(_filter, token, type, size)
+
+def build_article_query(_filter: str, token: str, type: str = "query", size: int = 50):
+    _filter = "type:Article " + _filter
+    return build_knowledge_graph_query(_filter, token, type, size)
+
+def build_extract_article_query(_filter: str, token: str, type: str = "query"):
+    raise NotImplementedError("build_extract_article_query not yet implemented.")
